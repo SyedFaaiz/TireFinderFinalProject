6b5aca45cbc6ff55564126c04987918b46073916
faaiz rehan <faaizrehan@hotmail.com>
2020-12-18 20:00:24 -0500

    Add project files.

---
+++ TireFinderFinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TireFinderFinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
